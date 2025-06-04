093e3ea2e5e8c1466bc7fef47fb4918d4551cda4
Camellia-y2 <3220106922@qq.com>
2025-06-04 21:34:25 +0800

    添加了react学习

---
+++ react/todos/src/App.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react'
+import './App.css'
+// todos 列表需要渲染
+// 函数组件 App组件 组合其他的组件完成应用
+// html css js 用函数组合在一起，
+//  function App()：函数组件
+function App() {
+  // react 比vue 更纯粹
+  // const todos = ['吃饭','睡觉','打豆豆'];// 数组 -》 数据
+
+  // 数据 -》 数据状态 数据业务 改变的 数据状态
+  const [todos, setTodos] = useState(['吃饭','睡觉','打豆豆']);
+  const [title,setTitle] = useState('ECUT 一明')
+  setTimeout(()=>{
+    // todos = ['吃饭','睡觉','打豆豆','养鱼'];
+    setTodos(['吃饭','睡觉','打豆豆','养鱼']);
+    setTitle('字节 一明')
+  },3000)
+  setTimeout(()=>{
+    // todos = ['吃饭','睡觉','打豆豆','养鱼'];
+    // setTodos(['吃饭','睡觉','打豆豆','养鱼']);
+    setTitle('北邮 一明')
+  },5000)
+  return (
+    <div>
+      <h1 class="title">{title}</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>序号</th>
+            <th>任务</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+             todos.map((item,index)=>(
+              <tr>
+                <td>{index+1}</td>
+                <td>{item}</td>
+              </tr>
+              )
+            )
+          }
+        </tbody>
+      </table>
+    </div>
+
+    // <>
+    //   <table>
+    //     <thead>
+    //       <tr>
+    //         <th>序号</th>
+    //         <th>任务</th>
+    //       </tr>
+    //       </thead>
+    //     <tbody>
+    //       {
+    //         //表示动态区域 用js
+    //         // react一个括号
+    //         // js DOM 编程API
+    //         // 在html里写 js 代码
+    //         todos.map((item,index)=>(
+    //           <tr>
+    //             <td>{index+1}</td>
+    //             <td>{item}</td>
+    //           </tr>
+    //           )
+    //         )
+    //       }
+    //     </tbody>
+    //   </table>
+    // </>
+  )
+}
+
+export default App
