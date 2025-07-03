bb6a15e5f5b761ba926415b56c8f84998c11bdfc
y2t <3220106922@qq.com>
2025-07-03 17:09:54 +0800

    添加cookie学习，实现前后端联调

--- js/storage/cookie-demo/public/script.js
+++ js/storage/cookie-demo/public/script.js
@@ -1,1 +1,42 @@
-console.log("hhhhh")
+// console.log("智能前端，智能后端，笑傲秋招")
+const loginForm = document.getElementById('loginForm');
+loginForm.addEventListener('submit', async function(event) {
+    event.preventDefault(); // 阻止表单的默认提交行为
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+    console.log(username,password);
+    
+    try {
+        const response = await fetch('/login', {
+            method: 'POST', // 请求方法为POST
+            headers: { // 请求头，指定请求的内容类型为application/json
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ username, password }) // 请求体，将用户名和密码转换为JSON字符串,只能发送字符串，要序列化         
+        })
+        const data = await response.json(); // 解析响应体为JSON对象
+        console.log(data);
+    }catch (error) {
+        console.error('登录失败');
+    }
+})
+
+document.addEventListener('DOMContentLoaded',async () => { // 页面加载完成后执行
+    // 登录了吗
+    try {
+        const response = await fetch('/check-login');
+        const data = await response.json(); // 解析响应体为JSON对象
+        console.log(data);
+        if (data.loggedIn) {
+            document.getElementById('loginSection').style.display = 'none';
+            document.getElementById('welcomeSection').style.display = 'block';
+            document.getElementById('userDisplay').textContent = data.username;
+          } else {
+            document.getElementById('loginSection').style.display = 'block';
+            document.getElementById('welcomeSection').style.display = 'none';
+          }
+    } catch (error) {
+        console.error('获取用户信息失败');
+    }
+})
+   
--- js/storage/cookie-demo/server.js
+++ js/storage/cookie-demo/server.js
@@ -19,48 +19,87 @@ const http = require('http'); // 引入核心模块
 const fs = require('fs'); // 文件系统模块 file system
 const path = require('path'); // 路径模块 path
 const server = http.createServer((req, res) => { // 回调函数 箭头函数
-        //res.end('Hello World\n'); // 响应体
-        //http 基于请求响应的协议
-        // 为了资源
-        if(req.method=='GET' && (req.url=='/' || req.url=='/index.html')){ // 首页
-            // 异步 callback
-            fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
-                // 前端体验为主，后端稳定为主
-                if(err){
-                    res.writeHead(500);//文件不存在 5xx-》服务器错误
-                    res.end('Server Error');
-                    return;
-                }
-                // 返回给html的内容，不只是html,css,js,jpg
-                res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'}); // 响应头
-                res.end(content); // 响应体
+    //res.end('Hello World\n'); // 响应体
+    //http 基于请求响应的协议
+    // 为了资源
+    if(req.method=='GET' && (req.url=='/' || req.url=='/index.html')){ // 首页
+        // 异步 callback
+        fs.readFile(path.join(__dirname,'public','index.html'),(err,content)=>{
+            // 前端体验为主，后端稳定为主
+            if(err){
+                res.writeHead(500);//文件不存在 5xx-》服务器错误
+                res.end('Server Error');
+                return;
+            }
+            // 返回给html的内容，不只是html,css,js,jpg
+            res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'}); // 响应头
+            res.end(content); // 响应体
+        })
+    }
+    // 后端路由，资源暴露
+    // http://localhost:1234/style.css
+    // 协议 http:// 域名 localhost 端口 1234 路径 /style.css 
+    if(req.method=='GET'&& req.url=='/style.css'){
+        fs.readFile(path.join(__dirname,'public','style.css'),(err,content)=>{
+            if(err){
+                res.writeHead(500);
+                res.end('Server Error');
+                return;
+            }
+            res.writeHead(200,{'Content-Type':'text/css;charset=utf-8'});
+            res.end(content);
+        })
+    }
+    if(req.method=='GET'&& req.url=='/script.js'){
+        fs.readFile(path.join(__dirname,'public','script.js'),(err,content)=>{
+            if(err){
+                res.writeHead(500);
+                res.end('Server Error');
+                return;
+            }
+            res.writeHead(200,{'Content-Type':'text/javascript;charset=utf-8'});
+            res.end(content);
+        })
+    }
+
+    if(req.method == 'POST' && req.url == '/login'){
+        res.writeHead(200,{
+            // 服务器端设置的 
+            // 在请求头设置cookie，浏览器会自动把cookie保存起来，下次请求的时候会自动带上cookie
+            'Set-Cookie': "user=admin;",
+            'Content-Type': "application/json"
+        }); // 响应头
+        res.end(
+            JSON.stringify({
+                success: true,
+                msg: "登录成功"
             })
+        ); // 登录成功 
+    }
+
+    if(req.method == 'GET' && req.url == '/check-login'){
+        // 登录了吗
+        console.log(req.headers.cookie);
+        if(req.headers.cookie){ // 登录成功
+            res.writeHead(200,{
+                'Content-Type': "application/json"
+            }); // 响应头
+            res.end(
+                JSON.stringify({
+                    loggedIn: true,
+                    username: "admin"
+                })
+            ); 
+            return;
+        }else{
+            res.end(
+                JSON.stringify({
+                    loggedIn: false,
+                    username: ""
+                })
+            ); 
         }
-        // 后端路由，资源暴露
-        // http://localhost:1234/style.css
-        // 协议 http:// 域名 localhost 端口 1234 路径 /style.css 
-        if(req.method=='GET'&& req.url=='/style.css'){
-            fs.readFile(path.join(__dirname,'public','style.css'),(err,content)=>{
-                if(err){
-                    res.writeHead(500);
-                    res.end('Server Error');
-                    return;
-                }
-                res.writeHead(200,{'Content-Type':'text/css;charset=utf-8'});
-                res.end(content);
-            })
-        }
-        if(req.method=='GET'&& req.url=='/script.js'){
-            fs.readFile(path.join(__dirname,'public','script.js'),(err,content)=>{
-                if(err){
-                    res.writeHead(500);
-                    res.end('Server Error');
-                    return;
-                }
-                res.writeHead(200,{'Content-Type':'text/javascript;charset=utf-8'});
-                res.end(content);
-            })
-        }
-    }); 
+    }
+}); 
 
 server.listen(1234); // 监听端口
