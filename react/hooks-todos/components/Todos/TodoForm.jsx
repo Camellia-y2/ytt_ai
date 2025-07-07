ddb1c5c9b2689aa60888ac55f429c7ce61ba5aa1
y2t <3220106922@qq.com>
2025-07-07 21:32:41 +0800

    react基础:hooks-todos-1

---
+++ react/hooks-todos/components/Todos/TodoForm.jsx
@@ -0,0 +1,17 @@
+import {
+    useState
+} from 'react'
+const TodoForm = () => {
+    // JSX 一定得有唯一的最外层元素 用div包裹  
+    // react用树状结构来编译解析JSX
+    return (
+        <>
+            <h1 className='header'>TodoList</h1>
+            <form action="">
+
+            </form>
+        </>
+    )
+}
+
+export default TodoForm
