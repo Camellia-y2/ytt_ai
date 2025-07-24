ddbd1504c513ee6c4cd95db56f07961183a3b656
y2t <3220106922@qq.com>
2025-07-24 21:29:46 +0800

    路由配置和Tabbar组件

---
+++ react/trip/src/components/BlankLayout/index.jsx
@@ -0,0 +1,15 @@
+import {
+    Outlet
+} from 'react-router-dom'
+const BlankLayout = () => {
+  return (
+    <>
+      <Outlet />
+      <div>
+        <h1>BlankLayout</h1>
+      </div>
+    </>
+  )
+}
+
+export default BlankLayout
---
+++ react/trip/src/pages/DisCount/index.jsx
@@ -0,0 +1,9 @@
+const DisCount = () => {
+  return (
+    <div>
+      <h1>DisCount</h1>
+    </div>
+  )
+}
+
+export default DisCount
